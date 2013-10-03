61e65bf22f5850be2d4c5d1efd4e26e0261b283b
Apprentice <apprentice@dbc01.local>
2013-10-02 20:21:45 -0700

    first working commit

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -2,8 +2,19 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to 
 
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration ## CREATED TABLE
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date :birthday
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
   end
 end
+
+## Into our SQLite DB file
