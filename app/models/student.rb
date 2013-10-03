83d75e9e60fa0142d6442011c78eba03244940d4
Apprentice <apprentice@dbc01.local>
2013-10-02 22:32:23 -0700

    added validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /[\w\.\-]+@\S+\.[a-zA-Z]+{2,}/ }, :uniqueness => true
+  validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+  validate :check_phone_number
+
   def name
     self[:first_name] + " " + self[:last_name]
   end
@@ -9,4 +13,10 @@ class Student < ActiveRecord::Base
     (Date.today.year - self[:birthday].year)
   end
 
+  def check_phone_number
+    if phone.gsub(/[^\d]/,'').length < 10
+      errors.add(:phone, 'Enter a valid phone number' )
+    end
+  end
+  
 end
